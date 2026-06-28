d30e18d3ff3ddcee503ca8dec5f177c5c32b2d7a
heavtek <heaventek13@gmail.com>
2026-06-28 17:01:13 +0300

    ride request accept logic

---
+++ src/routes/rideRoutes.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  becomeDriver,
+  toggleDriverStatus,
+  updateLocation,
+  findDrivers,
+  acceptRide,
+  requestRide,
+} from "../controllers/driverController.js";
+
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+// driver
+router.post("/driver/become", becomeDriver);
+router.patch("/driver/status", toggleDriverStatus);
+router.patch("/driver/location", updateLocation);
+
+// ride
+router.post("/ride/request", requestRide);
+router.get("/ride/drivers", findDrivers);
+router.post("/ride/accept", acceptRide);
+
+export default router;
